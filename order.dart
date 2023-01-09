8ae23e78c0c1d489d66007ebbefe568923989caa
goranklement <93207730+goranklement@users.noreply.github.com>
2023-01-09 20:56:32 +0100

    Add files via upload
    Realtime orders implemented

--- order.dart
+++ order.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
@@ -11,23 +14,33 @@ class OrderView extends StatefulWidget {
 
   @override
   State<OrderView> createState() => _OrderViewState();
+  
 }
 
 class _OrderViewState extends State<OrderView> {
   List<Object> orders=[];
   List<Widget> widgeti=[];
+  Timer? timer;
+  final _ordersRef= FirebaseDatabase.instance.ref('Orders');
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    timer?.cancel();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
    
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.numbers.toString()),
+        title: Text("List of orders"),
       ),
      body: Column(
       children: [
         ElevatedButton(onPressed: () async {
             orders=[];
+            widgeti.clear();
           for(int i=0;i<widget.numbers.length;i++){
             String broj= widget.numbers[i].toString();
             DatabaseReference starCountRef =
@@ -38,14 +51,34 @@ class _OrderViewState extends State<OrderView> {
                 setState(() {
                   
                   orders.add(data);
+              //  widgeti.add(OrderModel(tableNumber: broj, order: data.toString()));
                 });
                 
             });
                
           }
 }, child: Text("Get orders")),
-        OrderModel(tableNumber: "1", order: "PAOSJDOPASDOPPDJADSOPASDJDPOAJSDPOAPDSJPSADJPOAJDSPJASPDOJPASDJOPASJDPOJASPDAAPOSDJPODSPJADSPOJODSJPAOAPSODJPOASDJPDJPOADSJPOSPAODSJPJOPASDOPJAPDSJ"),
-        //Column( children: orders.map((e) => Text(e.toString())).toList() )    
+        /*ListBody(
+          children: widgeti,
+        ) */
+
+        Expanded(child: FirebaseAnimatedList(query:_ordersRef , itemBuilder: ((context, snapshot, animation, index) { 
+          if(widget.numbers.contains(int.parse(snapshot.key.toString())))
+          {
+          return ListBody(
+                      children:[
+                        OrderModel(tableNumber: snapshot.key.toString(), order: snapshot.value.toString(), ikona: IconButton(color:Colors.white,iconSize: 40 ,icon: Icon(Icons.delete), onPressed: () => _ordersRef.child(snapshot.key!).remove(),),),
+                      ] 
+
+                  );
+
+          }
+ 
+        else return SizedBox(
+          width: 1,
+        );
+          
+        }))) 
       ],
       
      ),
--- orderModel.dart
+++ orderModel.dart
@@ -5,34 +5,78 @@ import 'package:flutter/src/widgets/framework.dart';
 
 import 'colors.dart';
 
-class OrderModel extends StatelessWidget {
-  const OrderModel({super.key, required this.tableNumber, required this.order});
+class OrderModel extends StatefulWidget {
+  const OrderModel({super.key, required this.tableNumber, required this.order,  required this.ikona});
   final String tableNumber;
   final String order;
+  final IconButton ikona;
+
+  @override
+  State<OrderModel> createState() => _OrderModelState();
+  
+}
+
+
+class _OrderModelState extends State<OrderModel> with SingleTickerProviderStateMixin {
+   late AnimationController _animationController;
+  late Animation _animation;
+  @override
+  void dispose() {
+    _animationController.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(minutes: 10));
+
+    _animation = ColorTween(begin: getColor(0xff51b06d), end: getColor(0xffe80909))
+        .animate(CurvedAnimation(
+            curve: Curves.decelerate, parent: _animationController))
+          ..addListener(() {
+            setState(() {});
+          });
+
+    _animationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _animationController.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        _animationController.repeat(max: 1);
+        _animationController.forward();
+      }
+    });
+    _animationController.forward();
+  }
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return AnimatedContainer(
+      duration: Duration(minutes: 10),
       margin: EdgeInsets.all(7),
      padding: const EdgeInsets.all(20),
       decoration: BoxDecoration(
-        color: getColor(0xffba76cf),
+        color: _animation.value,
         borderRadius: BorderRadius.circular(30)
       ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [Text(tableNumber, style: TextStyle(
-            color: getColor(0xff716575),
-            fontWeight: FontWeight.bold,
-            fontSize: 40,
-          )),
-          SizedBox(width: 15,),
-          Expanded(
-            child: Text(order,style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 14,
-          )),
-          )],
+        child: InkWell(
+          onDoubleTap: (){print("deleted");},
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [Text(widget.tableNumber, style: TextStyle(
+              color: getColor(0xff716575),
+              fontWeight: FontWeight.bold,
+              fontSize: 40,
+            )),
+            SizedBox(width: 15,),
+            Expanded(
+              child: Text(widget.order,style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 14,
+            )),
+            ),
+            widget.ikona,]
+          ),
         ),
     );
   }
